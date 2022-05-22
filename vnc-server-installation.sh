a4756eeeaed2e79635acab9e208752d94a3c4660
replgithubAiden <92238811+replgithubAiden@users.noreply.github.com>
2022-05-22 10:36:51 -0400

    Create vnc-server-installation.sh

---
+++ vnc-server-installation.sh
@@ -0,0 +1,26 @@
+sudo apt-get update
+sudo apt-get install toilet figlet
+clear
+figlet -f big "VNC Server"
+echo hello and welcome to the kali script online
+python3 verify.py
+sudo apt update
+
+sudo apt install -y git
+
+git clone https://github.com/MysticRyuujin/guac-install.git /tmp/guac-install
+
+sudo rm /etc/localtime && sudo ln -s /usr/share/zoneinfo/US/Central /etc/localtime
+cd /tmp/guac-install/
+sudo ./guac-install.sh --nomfa --installmysql --mysqlpwd S3cur3Pa$$w0rd --guacpwd P@s$W0rD
+systemctl status tomcat9 guacd mysql
+sudo ss -antup | grep "mysqld\|guacd\|java"
+sudo apt install -y tigervnc-standalone-server
+mkdir -p ~/.vnc/
+wget https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-project/-/raw/master/nethunter-fs/profiles/xstartup -O ~/.vnc/xstartup
+vncserver :1
+echo server has been launched !!!!!
+echo github : replgithubaiden
+echo email  : aiden@suibhne.family
+echo email2 : githubaiden@hotmail.com
+echo email3 : alersa@protonmail.com
